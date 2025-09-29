099cc848891130c881dce0c1ccb7f5119681e9a0
mahdiekatebi <mahdiiekateb>
2025-09-29 09:46:22 +0330

    fix bug select yeaer and change language ,select type datepicker

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,113 @@
 <script setup>
-import {ref,defineModel} from "vue";
+import { ref } from "vue";
 import DateObject from "react-date-object";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import persian_en from "react-date-object/locales/persian_en";
+import gregorian_fa from "react-date-object/locales/gregorian_fa";
+import gregorian_en from "react-date-object/locales/gregorian_en";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Calendar from "./components/Calendar.vue";
 
+let type = ref("persian");
+let language = ref("fa");
+const date = ref("");
 
-let date = new DateObject();
-
-let type=ref("persian")
-let language=ref("fa")
-
-
-
-
-const msg = ref('')
-
-
-// dateFormat(type,{ year: 1404,
-//   month:1,
-//   day: 1,})
-
-
-
-
-    function handleInput(event) {
-      type.value = event.target.value;
-      // dateFormat(type, {
-      //   year:current_year.value,
-      //   month:event.target.value,
-      //   day: 1,
-      // })
-    }
+function handleInput(event) {
+  type.value = event.target.value;
+}
 
 function handleInputLan(event) {
   language.value = event.target.value;
-  // dateFormat(type, {
-  //   year:current_year.value,
-  //   month:event.target.value,
-  //   day: 1,
-  // })
 }
-
-
 </script>
 
 <template>
-  <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      نوع تقویم
-    </button>
-    <ul class="dropdown-menu">
-      <li>
-        <div class="form-check text-start">
-          <input value="gregorian" class="form-check-input" type="radio" name="radioDefault" id="radioDefault"
-                 checked  @input="handleInput"/>
-          <label class="form-check-label" for="radioDefault">
-         میلادی
-          </label>
-        </div>
-      </li>
-      <li>
-        <div class="form-check text-start">
-          <input value="persian" class="form-check-input" type="radio" name="radioDefault" id="radioDefault"
-                 checked  @input="handleInput"/>
-          <label class="form-check-label" for="radioDefault">
-       شمسی
-          </label>
-        </div>
-      </li>
-    </ul>
+  <div class="d-flex gap-2">
+    <div class="dropdown">
+      <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+      >
+        نوع تقویم
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <div class="form-check text-start">
+            <input
+                value="gregorian"
+                class="form-check-input"
+                type="radio"
+                name="radioType"
+                id="radioGregorian"
+                @input="handleInput"
+            />
+            <label class="form-check-label" for="radioGregorian">
+              میلادی
+            </label>
+          </div>
+        </li>
+        <li>
+          <div class="form-check text-start">
+            <input
+                value="persian"
+                class="form-check-input"
+                type="radio"
+                name="radioType"
+                id="radioPersian"
+                checked
+                @input="handleInput"
+            />
+            <label class="form-check-label" for="radioPersian"> شمسی </label>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="dropdown">
+      <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+      >
+        زبان
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <div class="form-check text-start">
+            <input
+                value="fa"
+                class="form-check-input"
+                type="radio"
+                name="radioLang"
+                id="radioFa"
+                checked
+                @input="handleInputLan"
+            />
+            <label class="form-check-label" for="radioFa"> fa </label>
+          </div>
+        </li>
+        <li>
+          <div class="form-check text-start">
+            <input
+                value="en"
+                class="form-check-input"
+                type="radio"
+                name="radioLang"
+                id="radioEn"
+                @input="handleInputLan"
+            />
+            <label class="form-check-label" for="radioEn"> en </label>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 
-
-  <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-     زبان
-    </button>
-    <ul class="dropdown-menu">
-      <li>
-        <div class="form-check text-start">
-          <input value="fa" class="form-check-input" type="radio" name="radioDefault" id="radioDefault"
-                 checked  @input="handleInputLan"/>
-          <label class="form-check-label" for="radioDefault">
-            fa
-          </label>
-        </div>
-      </li>
-      <li>
-        <div class="form-check text-start">
-          <input value="en" class="form-check-input" type="radio" name="radioDefault" id="radioDefault"
-                 checked  @input="handleInputLan"/>
-          <label class="form-check-label" for="radioDefault">
-            en
-          </label>
-        </div>
-      </li>
-    </ul>
-  </div>
-
-<Calendar :type="type" :lan="language" year="1404" month="6" v-model="msg"/>
-  <div>{{msg}}</div>
-<!--<SelectMonth/>-->
+  <Calendar :type="type" :lan="language" v-model="date" />
+  <div>{{ date }}</div>
 </template>
 
-<style>
-
-
-
-</style>
+<style></style>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,57 +1,62 @@
 <script setup>
-import {ref, watch, defineModel} from "vue";
+import { ref, watch, defineModel } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import modal from "bootstrap/js/src/modal.js";
 import DateObject from "react-date-object";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 import persian_en from "react-date-object/locales/persian_en";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-import modal from "bootstrap/js/src/modal.js";
-
+import gregorian_fa from "react-date-object/locales/gregorian_fa";
+import gregorian_en from "react-date-object/locales/gregorian_en";
 
 let date = new DateObject();
-let weekDays = ref([])
-let lenMon = ref([])
-let current_year = ref()
-let current_month = ref()
-let current_day = ref()
-let firstDayOfMonth = ref()
-let firstSelect = ref()
-let lastSelect = ref()
-let selected_day = ref()
-let monList = ref([])
-let labelMonths = ref("")
+let weekDays = ref([]);
+let lenMon = ref([]);
+let current_year = ref();
+let current_month = ref();
+let current_day = ref();
+let firstDayOfMonth = ref();
+let firstSelect = ref();
+let lastSelect = ref();
+let monList = ref([]);
+let selectMonth = ref("");
+let monthName = ref("");
 let current_date = ref({
   year: "",
   month: "",
-  day: ""
-})
+  day: "",
+});
+let inputYear = ref("");
+const props = defineProps(["type", "lan"]);
 
-
-const props = defineProps(['type', 'lan'])
-
-
-let model = defineModel()
-
-
+let model = defineModel();
 
 function createCalendar() {
-  const day = document.querySelectorAll("#days")
-  console.log(props.lan)
-  for (let i = 0; i < day.length; i++) {
-    day[i].classList.remove("selected");
-    day[i].classList.remove("firstDay");
-    day[i].classList.remove("lastDay");
+  const days = document.querySelectorAll("#days");
+  for (let i = 0; i < days.length; i++) {
+    days[i].classList.remove("selected");
+    days[i].classList.remove("firstDay");
+    days[i].classList.remove("lastDay");
 
+    if (days[i].classList.contains("currentDay")) {
+      days[i].style = "";
+    }
   }
+
   if (props.type === "persian") {
-    if (labelMonths.value.length != '' || (current_year.value !==  current_date.value.year)) {
+    if (
+        selectMonth.value != "" ||
+        current_year.value !== current_date.value.year
+    ) {
+      console.log(selectMonth.value, "month");
+
       date = new DateObject({
         date: new Date(),
         calendar: persian,
         locale: props.lan === "fa" ? persian_fa : persian_en,
         year: current_year.value,
-        month: labelMonths.value,
+        month: selectMonth.value,
         day: 1,
       });
     } else {
@@ -61,167 +66,216 @@ function createCalendar() {
         locale: props.lan === "fa" ? persian_fa : persian_en,
       });
     }
+    console.log(date);
 
+    current_day.value =
+        date.day === current_date.value.day &&
+        current_date.value.month === date.month.number
+            ? current_date.value.day
+            : date.day !== current_date.value.day &&
+            current_date.value.month === date.month.number
+                ? current_date.value.day
+                : "";
+    current_month.value = date.month.number;
+    current_year.value = date.year;
+    weekDays.value = date.weekDays;
+    lenMon.value = date.month.length;
+    monList.value = date.months;
+    selectMonth.value = date.month.number;
+    monthName.value = date.month.name;
 
-    current_day.value = date.day === current_date.value.day && current_date.value.month === date.month.number ? current_date.value.day :
-        date.day !== current_date.value.day && current_date.value.month === date.month.number ? current_date.value.day : ""
-    current_month.value = date.month.number
-    current_year.value = date.year
-    weekDays.value = date.weekDays
-    lenMon.value = date.month.length
-    monList.value = date.months
-    labelMonths.value = date.month.name
-    let {year, month, weekDay, day} = date.toFirstOfMonth()
-    firstDayOfMonth.value = weekDay.index
-
+    let { weekDay } = date.toFirstOfMonth();
+    firstDayOfMonth.value = weekDay.index;
   } else {
-    date = new DateObject({
-      date: new Date(),
-      // calendar: persian,
-      locale: persian_fa,
-      // year: data.year,
-      // month: data.month,
-      // day: data.day,
-    });
-    console.log(date, "date")
+    if (
+        selectMonth.value != "" ||
+        current_year.value !== current_date.value.year
+    ) {
+      date = new DateObject({
+        date: new Date(),
+        locale: props.lan === "fa" ? gregorian_fa : gregorian_en,
+        year: current_year.value,
+        month: selectMonth.value,
+        day: 1,
+      });
+    } else {
+      console.log("else");
 
-    current_day.value = date.day === current_date.value.day && current_date.value.month === date.month.number ? current_date.value.day :
-        date.day !== current_date.value.day && current_date.value.month === date.month.number ? current_date.value.day : ""
-    current_month.value = date.month.number
-    current_year.value = date.year
-    weekDays.value = date.weekDays
-    lenMon.value = date.month.length
-    monList.value = date.months
+      date = new DateObject({
+        date: new Date(),
+        locale: props.lan === "fa" ? gregorian_fa : gregorian_en,
+      });
+    }
 
-    let {year, month, weekDay, day} = date.toFirstOfMonth()
-    firstDayOfMonth.value = weekDay.index
+    current_day.value =
+        date.day === current_date.value.day &&
+        current_date.value.month === date.month.number
+            ? current_date.value.day
+            : date.day !== current_date.value.day &&
+            current_date.value.month === date.month.number
+                ? current_date.value.day
+                : "";
+    current_month.value = date.month.number;
+    current_year.value = date.year;
+    weekDays.value = date.weekDays;
+    lenMon.value = date.month.length;
+    monList.value = date.months;
+    selectMonth.value = date.month.number;
+    monthName.value = date.month.name;
+
+    let { weekDay } = date.toFirstOfMonth();
+    firstDayOfMonth.value = weekDay.index;
   }
-
 }
 
+watch(
+    () => [props.type, props.lan],
+    () => {
+      if (props.type === "persian") {
+        date = new DateObject({
+          date: new Date(),
+          calendar: persian,
+          locale: props.lan === "fa" ? persian_fa : persian_en,
+        });
 
-watch(() => [props.type, props.lan], () => {
-  if (props.type === "persian") {
-    date = new DateObject({
-      date: new Date(),
-      calendar: persian,
-      locale: props.lan === "fa" ? persian_fa : persian_en,
-    });
-
-    current_date.value =
-        {
+        current_date.value = {
           year: date.year,
           month: date.month.number,
           day: date.day,
+        };
+        current_year.value = date.year;
+        current_month.value = date.month.number;
+        selectMonth.value = date.month.number;
+        monthName.value = date.month.name;
+      } else {
+        date = new DateObject({
+          date: new Date(),
+          locale: props.lan === "fa" ? gregorian_fa : gregorian_en,
+        });
+        current_date.value = {
+          year: date.year,
+          month: date.month.number,
+          day: date.day,
+        };
 
-        }
-    current_year.value=date.year
-  } else {
-    current_year.value=date.year
-    date = new DateObject({
-      date: new Date(),
-    });
-    current_date.value = [
-      {
-        year: date.year,
-        month: date.month.number,
-        day: date.day,
-
+        current_year.value = date.year;
+        current_month.value = date.month.number;
+        selectMonth.value = date.month.number;
+        monthName.value = date.month.name;
       }
-    ]
-  }
-  createCalendar()
-}, {immediate: true});
-
+      createCalendar();
+    },
+    { immediate: true }
+);
 
 function click_day(e) {
-  model.value = ''
-  const day = document.querySelectorAll("#days")
+  model.value = "";
+  const days = document.querySelectorAll("#days");
   if (!firstSelect.value) {
-    for (let i = 0; i < day.length; i++) {
-      day[i].classList.remove("selected");
-      day[i].classList.remove("firstDay");
-      day[i].classList.remove("lastDay");
-      if(day[i].classList.contains("currentDay")) {
-        day[i].style.borderRadius="9px";
-        day[i].style.color="white";
-      }
-
-    }
-    firstSelect.value = e
-    for (let i = 0; i < day.length; i++) {
-      if (Number(day[i].getAttribute("day")) === e) {
-        day[i].classList.add("firstDay");
+    for (let i = 0; i < days.length; i++) {
+      days[i].classList.remove("selected");
+      days[i].classList.remove("firstDay");
+      days[i].classList.remove("lastDay");
+      if (days[i].classList.contains("currentDay")) {
+        days[i].style.borderRadius = "9px";
+        days[i].style.color = "white";
+        days[i].style.background = "#0269b8";
       }
     }
-
+    firstSelect.value = e;
+    for (let i = 0; i < days.length; i++) {
+      if (Number(days[i].getAttribute("day")) === e) {
+        days[i].classList.add("firstDay");
+      }
+    }
   } else {
-    lastSelect.value = e
-    for (let i = 0; i < day.length; i++) {
-      if (Number(day[i].getAttribute("day")) === e) {
-        day[i].classList.add("lastDay");
+    lastSelect.value = e;
+    for (let i = 0; i < days.length; i++) {
+      if (Number(days[i].getAttribute("day")) === e) {
+        days[i].classList.add("lastDay");
       }
     }
   }
 
   if (firstSelect.value && lastSelect.value) {
-    range_select(firstSelect.value, lastSelect.value)
+    range_select();
   }
-
 }
 
-function range_select(f, l) {
-  const day = document.querySelectorAll("#days")
-  for (let i = 0; i < day.length; i++) {
-    if (day[i].getAttribute("day") >= firstSelect.value && day[i].getAttribute("day") <= lastSelect.value) {
-      day[i].classList.add("selected")
-     if(day[i].classList.contains("currentDay")) {
-       day[i].style.borderRadius=0;
-       day[i].style.color="black";
-     }
+function range_select() {
+  const days = document.querySelectorAll("#days");
+  for (let i = 0; i < days.length; i++) {
+    if (
+        days[i].getAttribute("day") >= firstSelect.value &&
+        days[i].getAttribute("day") <= lastSelect.value
+    ) {
+      days[i].classList.add("selected");
+      if (days[i].classList.contains("currentDay")) {
+        days[i].style.borderRadius = 0;
+        days[i].style.color = "black";
+        days[i].style.background = "#c8c8c84a";
+      }
     }
   }
 
-  model.value = current_year.value + "/" + current_month.value + "/" + firstSelect.value + "to" + current_year.value + "/" + current_month.value + "/" + lastSelect.value
-  firstSelect.value = null
-  lastSelect.value = null
+  model.value =
+      current_year.value +
+      "/" +
+      current_month.value +
+      "/" +
+      firstSelect.value +
+      "to" +
+      current_year.value +
+      "/" +
+      current_month.value +
+      "/" +
+      lastSelect.value;
+  firstSelect.value = null;
+  lastSelect.value = null;
 }
 
-function selectMonth(event) {
-  console.log('update:modelValue', event.target.value);
-  labelMonths.value = event.target.value;
-  createCalendar()
-
+function selectedMonth(event) {
+  console.log("update:modelValue", event.target.value);
+  selectMonth.value = event.target.value;
+  createCalendar();
 }
 
-
-function go_year(year){
-  console.log(year,"vallue")
+function go_year() {
+  if (inputYear.value) {
+    current_year.value = Number(inputYear.value);
+    createCalendar();
+  }
 }
 
 function handleYear(event) {
-  current_year.value = event.target.value
+  inputYear.value = event.target.value;
 }
-
-
-
 </script>
 
 <template>
   <div class="d-flex justify-content-around">
-
-
-<!--    <button>‹</button>-->
-    <div class="d-flex gap-2">
+    <div class="d-flex gap-2 my-4">
       <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          {{ labelMonths }}
+        <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+          {{ monthName }}
         </button>
         <ul class="dropdown-menu">
           <li v-for="mon in monList">
             <div class="form-check text-start">
-              <input :value="mon.number" class="form-check-input" type="radio" name="radioDefault" id="radioDefault"
-                     checked @input="selectMonth"/>
+              <input
+                  :value="mon.number"
+                  class="form-check-input"
+                  type="radio"
+                  name="radioDefault"
+                  id="radioDefault"
+                  checked
+                  @input="selectedMonth"
+              />
               <label class="form-check-label" for="radioDefault">
                 {{ mon.name }}
               </label>
@@ -229,37 +283,49 @@ function handleYear(event) {
           </li>
         </ul>
       </div>
-      <div class="dropdown" >
-        <button class="btn btn-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          {{current_year}}
+      <input v-model="model" readonly />
+      <div class="dropdown">
+        <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+          {{ current_year }}
         </button>
         <ul class="dropdown-menu">
           <li class="enter_year">
-            <input :value="current_year" @input="handleYear"/>
-            <button type="button" class="btn_year" @click="go_year(current_year)">برو به سال</button>
+            <input :value="inputYear" @input="handleYear" />
+            <button type="button" class="btn_year" @click="go_year()">
+              برو به سال
+            </button>
           </li>
         </ul>
       </div>
     </div>
-<!--    <button>›</button>-->
   </div>
   <div class="container text-center" style="width: 23rem">
-    <input v-model="model" readonly>
     <div class="day_grid">
       <div class="col" v-for="w in weekDays">
-        {{ w.name }}
+        {{ w.shortName }}
       </div>
-
     </div>
     <div class="day_grid">
-      <div v-for="day in firstDayOfMonth">
-      </div>
-      <div id="days" :day="d" v-for="d in lenMon" key="d" @mousedown="click_day(d)"
-           :class="[d === current_day ? 'currentDay':'custom',(firstDayOfMonth + (d)) % 7 === 0   ? 'friday' :'']">
+      <div v-for="day in firstDayOfMonth"></div>
+      <div
+          id="days"
+          :day="d"
+          v-for="d in lenMon"
+          key="d"
+          @mousedown="click_day(d)"
+          :class="[
+          d === current_day ? 'currentDay' : 'custom',
+          (firstDayOfMonth + d) % 7 === 0 ? 'friday' : '',
+        ]"
+      >
         {{ d }}
       </div>
     </div>
-
   </div>
 </template>
 
@@ -279,8 +345,7 @@ function handleYear(event) {
 .currentDay {
   background: #0269b8;
   color: white;
-border-radius: 9px;
-
+  border-radius: 9px;
 }
 
 .selected {
@@ -292,7 +357,6 @@ border-radius: 9px;
   color: white;
   border-bottom-right-radius: 10px;
   border-top-right-radius: 10px;
-
 }
 
 .lastDay {
@@ -304,26 +368,24 @@ border-radius: 9px;
 
 .friday {
   color: #ff0000;
-
 }
-.enter_year{
+.enter_year {
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 20px;
   padding: 0.2rem;
 }
-.btn_year{
+.btn_year {
   height: 30px;
   width: max-content;
 }
-#days{
-  width:100%;
+#days {
+  width: 100%;
   height: 3rem;
   text-align: center;
   align-items: center;
   display: flex;
   justify-content: center;
-
 }
 </style>
